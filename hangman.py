c378aa73f6370431169742f13ec56b6ebe4a546b
Tomio Matsushita <matheustcm@hotmail.com>
2022-08-16 16:44:16 +0900

    Update hangman.py
    Code implementation to limit user attempts.
    Loop code improvement

--- hangman.py
+++ hangman.py
@@ -3,28 +3,39 @@ def play():
     print("✝✝✝Welcome to Hangman Game!✝︎✝︎✝︎")
     print("✝︎✝︎✝︎✝︎✝︎✝︎✝︎✝︎✝︎✝︎✝︎✝︎✝︎✝︎✝︎✝︎✝︎✝︎✝︎✝︎✝︎✝︎✝︎✝︎✝︎✝︎✝✝︎✝︎✝︎✝︎")
 
-    secret_word = "banana"
-    hits_letters = ["_","_","_","_","_","_"]
+    secret_word = "banana".upper()
+    hit_letter = ["_","_","_","_","_","_"]
 
     wrong = False
     right = False
+    trys = 0
 
-    print(hits_letters)
+    print(hit_letter)
 
     while(not wrong and not right):
 
         shot = input("Type a letter...")
-        shot = shot.strip()
+        shot = shot.strip().upper()
 
-        index = 0
-        for letter in secret_word:
-            if(shot.upper() == letter.upper()):
-                hits_letters[index] = letter
-            index = index + 1    
+        if(shot in secret_word):
+            index = 0
+            for letter in secret_word:
+                if(shot == letter):
+                    hit_letter[index] = letter
+                index += 1    
+        else:
+            trys += 1
 
-        print(hits_letters)
-
-    print("Game Over")
+        wrong = trys == 6
+        right = "_" not in hit_letter
+        print(hit_letter)
+    if(right):
+        print("Congratulations, you win!")
+    else:    
+        print("You exceeded the number of attempts!")
+        print("☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️")
+        print("☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️Game Over☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️")
+        print("☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️")
 
 if(__name__ == "__main__"):
     play()    
